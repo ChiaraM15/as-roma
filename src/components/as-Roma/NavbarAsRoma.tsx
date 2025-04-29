b16901cad8187daca8d84732b835a845e79520f2
chiara mingolla <chiaramingolla@gmail.com>
2025-04-29 14:50:25 +0200

    navbar roma

--- src/components/as-Roma/NavbarAsRoma.tsx
+++ src/components/as-Roma/NavbarAsRoma.tsx
@@ -1,18 +1,28 @@
 import React from "react";
-import roma from "../../assets/as-roma-final.png";
-import menu from "../../assets/menu.png"
+import roma from "../../assets/as-roma.svg";
+import menu from "../../assets/menu.png";
+import riyadh from "../../assets/riyadh-white.png";
+import adidas from "../../assets/adidas.png";
 import { NavbarAsList, RightList } from "../../utils/asDati";
-import "./styles.css"
+import "./styles.css";
 
 const NavbarAsRoma = () => {
   return (
     <div className="navbar">
-      <div className="prima-riga"></div>
-      <div className="logo">
-        <img className="roma" src="roma" alt="roma" />
+      <div className="prima-riga">
+        <div className="logo">
+          <img className="roma" src={roma} alt={"roma"} />
+        </div>
+        <div className="prima-riga-destra">
+          <div className="sponsor">
+            <img className="riyadh" src={riyadh} alt={"riyadh"} />
+            <img className="adidas" src={adidas} alt={"adidas"} />
+          </div>
+        </div>
       </div>
+
       <div className="seconda-riga">
-        <img className="menu" src="menu" alt="menu" />
+        <img className="menu" src={menu} alt={"menu"} />
         {NavbarAsList.map((dati) => {
           return (
             <div key={dati.text} className="dati-item">
@@ -20,13 +30,15 @@ const NavbarAsRoma = () => {
             </div>
           );
         })}
-        <div className="seconda-riga-destra">{RightList.map((destra) =>{
-          return (
-            <div key={destra.text} className="destra-item">
-              {destra.text}
-            </div>
-          );
-        })}</div>
+        <div className="seconda-riga-destra">
+          {RightList.map((destra) => {
+            return (
+              <div key={destra.text} className="destra-item">
+                {destra.text}
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
--- src/utils/asDati.ts
+++ src/utils/asDati.ts
@@ -1,14 +1,14 @@
 export const NavbarAsList: Array<ElementAsRoma> = [
-  { id: 0, text: "Squadra" },
-  { id: 1, text: "Calendario" },
-  { id: 2, text: "Risultati" },
-  { id: 3, text: "Romanisti" },
-  { id: 4, text: "Notizie" },
+  { id: 0, text: "SQUADRA" },
+  { id: 1, text: "CALENDARIO" },
+  { id: 2, text: "RISULTATI" },
+  { id: 3, text: "ROMANISTI" },
+  { id: 4, text: "NOTIZIE" },
 ];
 
 export const RightList: Array<ElementAsRoma> = [
   { id: 0, text: "IT" },
-  { id: 1, text: "Bigletti" },
-  { id: 2, text: "Shop" },
-  { id: 3, text: "Accedi" },
+  { id: 1, text: "BIGLIETTI" },
+  { id: 2, text: "SHOP" },
+  { id: 3, text: "ACCEDI" },
 ];
